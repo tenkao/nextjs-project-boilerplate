283ee3f8a17aa721a6c0f8f38bb727117913cb5b
Hagiwara Tsutomu <tenkao@gmail.com>
2021-07-26 18:38:44 +0900

    ESLint の設定を追加

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // ビルド時にチェックしない
+    dirs: ['src/pages/', 'src/components/', 'src/lib/'], // 特定のディレクトリのみ対象にする
+  }
 }
