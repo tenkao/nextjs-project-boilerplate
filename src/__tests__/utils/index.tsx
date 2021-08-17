dc18d755f84c4fcb2898c23d464677ea0991c3d9
Hagiwara Tsutomu <tenkao@gmail.com>
2021-08-17 23:17:45 +0900

    React コンポーネントのスナップショットテストを書いてみる

--- src/__tests__/utils/index.tsx
+++ src/__tests__/utils/index.tsx
@@ -1,7 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { render } from '@testing-library/react'
 import React from 'react'
-import theme from 'src/theme'
+// import theme from 'src/theme'
+// 存在せずエラーになるのでとりあえずコメントアウト
 
 const AllTheProviders = ({ children }: { children: JSX.Element }) => {
   return <ChakraProvider>{children}</ChakraProvider>
